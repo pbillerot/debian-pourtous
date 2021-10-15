0ced115979310917e966b5359eb81492f63a2cb1
pbillerot <philippe.billerot@gmail.com>
2021-10-15 12:22:10 +0200

    chrome

--- debian-mini.sh
+++ debian-mini.sh
@@ -14,8 +14,8 @@ sudo flatpak install flathub com.github.jeromerobert.pdfarranger
 # flatpak install flathub com.vscodium.codium
 
 # Google Chrome
-sudo wget -qO - https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/googlechrome-linux-keyring.gpg
-sudo echo "deb [arch=amd64 signed-by=/usr/share/keyrings/googlechrome-linux-keyring.gpg] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+wget -qO - https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/googlechrome-linux-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/googlechrome-linux-keyring.gpg] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
 sudo apt-get update
 sudo apt-get install -y google-chrome-stable
 
