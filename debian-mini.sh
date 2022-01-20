42c9456e9ca0d92c17f82bbb142c5698b52a75a5
pbillerot <philippe.billerot@gmail.com>
2022-01-20 09:16:53 +0100

    LibreOffice remplacée par OnlyOffice

--- debian-mini.sh
+++ debian-mini.sh
@@ -11,3 +11,8 @@ apt install -y sshfs nfs-common xauth cifs-utils nnn
 apt install flatpak gnome-software-plugin-flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
+# Suppression de LibreOffice
+apt remove --purge libreoffice*
+
+# Installation de OnlyOffice
+flatpak install flathub org.onlyoffice.desktopeditors
