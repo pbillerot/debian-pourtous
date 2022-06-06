59fb4987d5b1974bd50d3bacac0e11de5e75d588
pbillerot <philippe.billerot@gmail.com>
2022-06-06 12:12:03 +0200

    Update debian-popos.sh

--- debian-popos.sh
+++ debian-popos.sh
@@ -8,6 +8,3 @@ apt install -y sshfs nfs-common xauth cifs-utils nnn
 # Suppression de LibreOffice .deb
 apt remove --purge libreoffice*
 
-# Installation de OnlyOffice
-flatpak install flathub org.onlyoffice.desktopeditors
-
