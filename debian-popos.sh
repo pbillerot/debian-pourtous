2ff23288c5abd1a2d1d4075557ea157b72315a29
pbillerot <philippe.billerot@gmail.com>
2022-06-06 12:12:17 +0200

    Update debian-popos.sh

--- debian-popos.sh
+++ debian-popos.sh
@@ -3,7 +3,7 @@
 # Ajouts de paquets standards
 apt install -y nautilus-image-converter
 apt install -y gthumb zim wget
-apt install -y sshfs nfs-common xauth cifs-utils nnn
+apt install -y sshfs nfs-common cifs-utils nnn
 
 # Suppression de LibreOffice .deb
 apt remove --purge libreoffice*
