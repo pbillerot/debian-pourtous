dc296a7dc2c80967d44352bbaf376b4b72299a4e
pbillerot <philippe.billerot@gmail.com>
2023-06-01 18:43:31 +0200

    manual-installed

--- debian-status.sh
+++ debian-status.sh
@@ -13,12 +13,6 @@ flatpak list >>DEBIAN-${USER}.TXT
 echo ***
 echo *** LOGICIELS APT : ${USER} >>DEBIAN-${USER}.TXT
 echo ***
-#apt list | grep [installé] >>DEBIAN-${USER}.TXT
+apt list --manual-installed=true >>DEBIAN-${USER}.TXT
 
-(zcat $(ls -tr /var/log/apt/history.log*.gz); cat /var/log/apt/history.log) 2>/dev/null |
-  egrep '^(Start-Date:|Commandline:)' |
-  grep -v aptdaemon |
-  egrep '^Commandline:'>>DEBIAN-${USER}.TXT
-
-echo ***
 cat DEBIAN-${USER}.TXT
