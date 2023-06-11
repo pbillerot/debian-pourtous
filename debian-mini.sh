6da705c056e6636160830ae95064c16012ab7fa8
pbillerot <philippe.billerot@gmail.com>
2023-06-11 09:48:39 +0200

    Update debian-mini.sh

--- debian-mini.sh
+++ debian-mini.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 # nous sommes en root
 # Ajouts de paquets standards
-# apt install gnome-shell-extension-dash-to-panel gnome-shell-extension-appindicator
-apt install gnome-shell-extension-dash-to-panel
+apt install gnome-shell-extension-dash-to-panel gnome-shell-extension-appindicator
 apt install -y nautilus-image-converter
-apt install -y gthumb zim wget htop nnn ncdu inxi
+apt install -y gthumb zim wget htop nnn ncdu
 apt install -y sshfs nfs-common xauth cifs-utils nnn
 # apt install -y inxi
 
