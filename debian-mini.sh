579504809b4e8b84f9bd2a19103efe786978e280
pbillerot <philippe.billerot@gmail.com>
2023-06-18 10:59:36 +0200

    install de chrome

--- debian-mini.sh
+++ debian-mini.sh
@@ -10,4 +10,9 @@ apt install -y sshfs nfs-common xauth cifs-utils nnn
 apt install flatpak gnome-software-plugin-flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
+# Installation de Chrome
+curl -fSsL https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor | tee /usr/share/keyrings/google-chrome.gpg >> /dev/null
+echo deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main | tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt update
+sudo apt install google-chrome-stable
 
